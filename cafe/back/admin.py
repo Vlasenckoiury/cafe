6548dd58b0bc849393801c9025a1c91403ebc307
iuryvlasenko <vlasencko.iury1999@yandex.by>
2025-03-09 14:30:42 +0300

    добавлена админка

--- cafe/back/admin.py
+++ cafe/back/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Cafe, MenuItem
 
-# Register your models here.
+
+@admin.register(Cafe)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('id', 'table_number', 'total_price', 'status')
+    list_filter = ('status',)
+    search_fields = ('id', 'table_number')
+
+
+@admin.register(MenuItem)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price')
